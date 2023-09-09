46e3386a4af7ff3b147ab0cb8ba81f79936ffcd0
Sanjana singh <sanjanasingh7774@gmail.com>
2023-09-09 19:27:40 +0530

    Time: 53 ms (56.07%), Space: 48.5 MB (27.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.size()<=1)return true;
-        int maxi(nums[0]);        
-        for(int i=1;i<nums.size()-1;i++){
-            if(maxi>0)maxi--;else return false;
-            maxi=max(nums[i],maxi);
+        int maxReach = 0;  
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxReach) {
+                return false;
+            }
+
+            maxReach = max(maxReach, i + nums[i]);
+
+            if (maxReach >= nums.size() - 1) {
+                return true;
+            }
         }
-        if(maxi<=0)return false;
-        return true;
+
+        return false;
     }
 };
