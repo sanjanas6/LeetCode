1e2f198b9f2767a5c4ab41e300f0854f8a5af68a
Sanjana singh <sanjanasingh7774@gmail.com>
2023-09-18 22:22:14 +0530

    Time: 67 ms (54.88%), Space: 159.9 MB (22.10%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    
+    // Function to check for palindromic substrings
+    int check(string s, int p1, int p2, int &st, int &en)
+    {
+        int count = 0;
+        
+        if (p1 == p2) {
+            count++;
+            p1--;
+            p2++;
+        }
+        
+        while (p1 >= 0 && p2 < s.length()) {
+            if (s[p1] != s[p2]) {
+                break;
+            }
+            
+            p1--;
+            p2++;
+            count += 2;
+        }
+        
+        st = p1 + 1;
+        en = p2 - 1;
+        return count; 
+    }
+    
+    string longestPalindrome(string s) {
+        int st = -1;
+        int en = -1;
+            
+        int len = 0;
+        
+        int fs = -1;
+        int fe = -1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            // For Odd Length
+            int x = check(s, i, i, st, en);
+            
+            if (x > len) {
+                len = x;
+                fs = st;
+                fe = en;
+            }
+            
+            // For Even Length
+            int y = check(s, i, i + 1, st, en);
+            
+            if (y > len) {
+                len = y;
+                fs = st;
+                fe = en;
+            }
+        }
+        
+        if (fs == -1 || fe == -1) {
+            // No palindrome found, return an empty string
+            return "";
+        }
+        
+        return s.substr(fs, len);
+    }
+};
